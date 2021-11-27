5817406366a9a7e0f368a65674ca0375a384d4a8
yu-0922 <yusuke.1989.t@gmail.com>
2021-11-27 13:11:04 +0900

    アニメーション追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card slide-in">
                 <div class="card-header">{{ __('ログイン画面') }}</div>
 
                 <div class="card-body">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card slide-in">
                 <div class="card-header">{{ __('会員登録') }}</div>
 
                 <div class="card-body">
