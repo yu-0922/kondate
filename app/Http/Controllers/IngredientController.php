934bf93ec185dffd214686c6b8d6813479a007c6
yu-0922 <yusuke.1989.t@gmail.com>
2021-12-22 15:10:01 +0900

    コメントアウト削除

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -3,44 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Facades\App\Models\Menu;
-use Facades\App\Models\User;
-use Facades\App\Models\Ingredient;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class IngredientController extends Controller
 {
-    public function create(Request $request)
-    {
-        // dd($theMenu);
-        // $ingredient = $request->get('id');;
-        // return view('ingredients.create',['theMenu' => $ingredient]);
-    }
-
-    public function store(Request $request)
-    {
-        //バリデーション
-        // $validatedData = $request->validate([
-        //     'menu_id' => 'required',
-        //     'ing_name.*' => 'required|max:3000',
-        //     'ing_size.*' => 'required|max:3000',
-        // ]);
-
-        // $datas = $request->get('ing_name');
-        // $sizes = $request->get('ing_size');
-        // $menu_id = $request->get('menu_id');
-
-        // foreach($datas as $key => $data) {
-        //     Ingredient::create([
-        //         'menu_id' => $menu_id,
-        //         'ingredient_name' => $data,
-        //         'unit' => $sizes[$key]
-        //     ]);
-        // }
-        // return redirect()->route('menu.index')->with('message', $validatedData['menu_name'] . 'を登録しました！');
-    }
-
     public function show()
     {
         // 今日を取得
